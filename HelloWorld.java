4740098f946ed3932cfdba740728f2ba5c975ba2
Pawan Kumar N <kumarpawan49995@gmail.com>
2025-05-10 12:40:51 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 package com.example;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public String sayHello() {
+        return "Hello, Jenkins!";
     }
 }
