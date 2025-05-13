880179467735788a1052d3e38fd5095afc4e97fc
Pawan Kumar N <kumarpawan49995@gmail.com>
2025-05-13 20:00:11 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,4 +4,9 @@ public class HelloWorld {
     public String sayHello() {
         return "Hello, Jenkins!";
     }
+
+    public static void main(String[] args) {
+        HelloWorld hw = new HelloWorld();
+        System.out.println(hw.sayHello());
+    }
 }
